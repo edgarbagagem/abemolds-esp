248ec422b3f5c7eac4e41e95e560bd8873cd3da7
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2024-01-05 18:21:17 +0000

    Added NTP support

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -7,6 +7,8 @@
 //Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 #include  "secrets.h"
+#include <TimeLib.h>
+#include <WiFiUdp.h>
 
 #define HTTP_REST_PORT 80
 
@@ -23,6 +25,15 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 ESP8266WebServer server(HTTP_REST_PORT);
+
+const int timeZone = 0;
+static const char ntpServerName[] = "pt.pool.ntp.org";
+String curDateStr = "";
+int today = 0;
+int prevDay = -1;
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
 
 int maxCavityTemp;
 int maxCoolingTemp;
@@ -86,6 +97,13 @@ void setup() {
   }
   Serial.print("Connected to WiFi, Local IP address ");
   Serial.println(WiFi.localIP());
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+  Serial.println("waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
 
   /* Assign the api key (required) */
   config.api_key = API_KEY;
@@ -114,6 +132,8 @@ void setup() {
   demoStageStartMillis = millis();
 
   updateCorrectParameters();
+
+  getDayStr();
 }
 
 void loop() {
@@ -122,6 +142,7 @@ void loop() {
     manageProduction();
   }
 }
+
 
 void demoMode() {
   //determine current stage's duration
@@ -519,4 +540,75 @@ void getManufacturingParameters() {
 
   Serial.print("MinFillMillis: ");
   Serial.println(minFillMillis);
+}
+
+void getDayStr() {
+  if (timeStatus() != timeNotSet) {
+    if (day() != today) { //update the display only if time has changed
+      prevDay = today;
+      today = day();
+      String dayStr = today < 10 ? String(0) + String(today) : String(today);
+      String monthStr = month() < 10 ? String(0) + String(month()) : String(month());
+      curDateStr = dayStr + monthStr + String(year());
+    }
+  }
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
